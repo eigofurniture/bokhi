fc42d5fc564a68e4534c1bdaef15ecd9badf0758
eigofurniture <162261950+eigofurniture@users.noreply.github.com>
2025-02-04 06:50:42 +0700

    Add files via upload

---
+++ components/ads.tsx
@@ -0,0 +1,7 @@
+import Script from 'next/script';
+
+const AdScript = () => (
+  <Script src="\/\/februaryinjectionproudly.com\/75\/71\/14\/7571143e30fdf9eee201ee7d5c03c91e.js"/>
+);
+
+export default AdScript;
